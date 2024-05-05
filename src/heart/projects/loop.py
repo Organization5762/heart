c336a618d87dc3ec8ebf7cd648e7a09fd5aa2377
wbrege <william.brege@gmail.com>
2024-05-05 16:03:22 -0700

    Search loop base

--- src/heart/projects/loop.py
+++ src/heart/projects/loop.py
@@ -1,30 +1,33 @@
 import os
+import logging
+import platform
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
-import logging
-from heart.display.renderers.kirby import KirbyRunning
+from heart.display.renderers.kirby_loop import KirbyLoop
 from heart.input.environment import GameLoop
 from heart.projects.rgb_display import LEDMatrix
-import platform
 
 logger = logging.getLogger(__name__)
+
 
 def run():
     devices = []
     if platform.system() == "Linux":
         devices.append(LEDMatrix())
-    
+    screen_width, screen_height = 256, 256
+
     loop = GameLoop(
-        64,
-        64,
+        screen_width,
+        screen_height,
         devices=devices
     )
     
     loop.add_renderer(
-        KirbyRunning()
+        KirbyLoop(screen_width, screen_height)
     )
     loop.start()
 
+
 if __name__ == "__main__":
     run()
-    
+
