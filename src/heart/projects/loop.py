077b6deddffe955f1db7a867a01734b4f0307e25
rasperry.pi <rasperry.pi@example.com>
2024-05-05 13:25:44 -0700

    Changes from the rasperry pi

--- src/heart/projects/loop.py
+++ src/heart/projects/loop.py
@@ -1,7 +1,7 @@
 import logging
 from heart.display.renderers.kirby import KirbyRunning
 from heart.input.environment import GameLoop
-from heart.projects.rgb_display import ImageScroller
+from heart.projects.rgb_display import LEDMatrix
 
 logger = logging.getLogger(__name__)
 
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     loop = GameLoop(
         64,
         64,
-        devices=[ImageScroller()]
+        devices=[LEDMatrix()]
     )
     
     loop.add_renderer(
--- src/heart/projects/rgb_display.py
+++ src/heart/projects/rgb_display.py
@@ -76,40 +76,12 @@ class SampleBase(object):
             sys.exit(0)
 
         return True
-
-def create_gradient(width, height):
-    # Create a new blank image
-    img = Image.new("RGB", (width, height), "#FFFFFF")
-    
-    # Load the pixel map
-    pixels = img.load()
-
-    # Generate gradient
-    for i in range(width):
-        # Calculate the intensity of the color
-        intensity = int(255 * (i / width))
-        for j in range(height):
-            # Set the color for each pixel
-            pixels[i, j] = (intensity, intensity, intensity)
-
-    return img
-
-class ImageScroller(SampleBase):
+class LEDMatrix(SampleBase):
     def __init__(self, *args, **kwargs):
-        super(ImageScroller, self).__init__(*args, **kwargs)
+        super(LEDMatrix, self).__init__(*args, **kwargs)
         
-        # self.parser.add_argument("--led-scan-mode", action="store", help="Progressive or interlaced scan. 0 Progressive, 1 Interlaced (default)", default=1, choices=range(2), type=int)
-        # self.parser.add_argument("--led-pwm-lsb-nanoseconds", action="store", help="Base time-unit for the on-time in the lowest significant bit in nanoseconds. Default: 130", default=130, type=int)
-        # self.parser.add_argument("--led-show-refresh", action="store_true", help="Shows the current refresh rate of the LED panel")
-        # self.parser.add_argument("--led-slowdown-gpio", action="store", help="Slow down writing to GPIO. Range: 0..4. Default: 1", default=1, type=int)
-        # self.parser.add_argument("--led-no-hardware-pulse", action="store", help="Don't use hardware pin-pulse generation")
-        # self.parser.add_argument("--led-rgb-sequence", action="store", help="Switch if your matrix has led colors swapped. Default: RGB", default="RGB", type=str)
-        # self.parser.add_argument("--led-pixel-mapper", action="store", help="Apply pixel mappers. e.g \"Rotate:90\"", default="", type=str)
-        # self.parser.add_argument("--led-row-addr-type", action="store", help="0 = default; 1=AB-addressed panels; 2=row direct; 3=ABC-addressed panels; 4 = ABC Shift + DE direct", default=0, type=int, choices=[0,1,2,3,4])
-        # self.parser.add_argument("--led-multiplexing", action="store", help="Multiplexing type: 0=direct; 1=strip; 2=checker; 3=spiral; 4=ZStripe; 5=ZnMirrorZStripe; 6=coreman; 7=Kaler2Scan; 8=ZStripeUneven... (Default: 0)", default=0, type=int)
-        # self.parser.add_argument("--led-panel-type", action="store", help="Needed to initialize special panels. Supported: 'FM6126A'", default="", type=str)
-        # self.parser.add_argument("--led-no-drop-privs", dest="drop_privileges", help="Don't drop privileges from 'root' after initializing the hardware.", action='store_false')
         options = RGBMatrixOptions()
+        # TODO: Might need to change these if we want N screens
         options.rows = 64
         options.cols = 64
         options.chain_length = 1
@@ -124,35 +96,6 @@ class ImageScroller(SampleBase):
         options.pixel_mapper_config = ""
         options.panel_type = ""
         options.gpio_slowdown = 4
-        # self.args = argparse.Namespace(led_rows=64, led_cols=64, led_chain=1, led_parallel=1, led_pwm_bits=11, led_brightness=100, led_gpio_mapping=None, led_scan_mode=1, led_pwm_lsb_nanoseconds=130, led_show_refresh=False, led_slowdown_gpio=4, led_no_hardware_pulse=None, led_rgb_sequence='RGB', led_pixel_mapper='', led_row_addr_type=0, led_multiplexing=0, led_panel_type='', drop_privileges=True)
-
-        # if self.args.led_gpio_mapping != None:
-        #   options.hardware_mapping = self.args.led_gpio_mapping
-          
-        # print(self.args.led_rows)
-        # options.rows = self.args.led_rows
-        # options.cols = self.args.led_cols
-        # options.chain_length = self.args.led_chain
-        # options.parallel = self.args.led_parallel
-        # options.row_address_type = self.args.led_row_addr_type
-        # options.multiplexing = self.args.led_multiplexing
-        # options.pwm_bits = self.args.led_pwm_bits
-        # options.brightness = self.args.led_brightness
-        # options.pwm_lsb_nanoseconds = self.args.led_pwm_lsb_nanoseconds
-        # options.led_rgb_sequence = self.args.led_rgb_sequence
-        # options.pixel_mapper_config = self.args.led_pixel_mapper
-        # options.panel_type = self.args.led_panel_type
-
-        # if self.args.led_show_refresh:
-        #   options.show_refresh_rate = 1
-
-        # if self.args.led_slowdown_gpio != None:
-        #     options.gpio_slowdown = self.args.led_slowdown_gpio
-        # if self.args.led_no_hardware_pulse:
-        #   options.disable_hardware_pulsing = True
-        # if not self.args.drop_privileges:
-        #   options.drop_privileges=False
-        
         # I hate this option.
         options.drop_privileges=False
         
@@ -172,6 +115,3 @@ class ImageScroller(SampleBase):
         self.offscreen_canvas.Clear()
         self.offscreen_canvas.SetImage(image, 0, 0)
         self.offscreen_canvas = self.matrix.SwapOnVSync(self.offscreen_canvas)
-
-        # self.double_buffer.SetImage(image)
-        # self.matrix.SwapOnVSync(self.double_buffer)
