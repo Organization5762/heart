a282ab10b4e960ca7b6d9ff6110bd23bfc112079
Michael Lampe <michaelthelampe@gmail.com>
2025-05-04 15:44:54 -0700

    fmt

--- src/heart/utilities/env.py
+++ src/heart/utilities/env.py
@@ -5,8 +5,8 @@ from dataclasses import dataclass
 from typing import Iterator
 
 import serial.tools.list_ports
-
 from pygame.event import custom_type
+
 
 @dataclass
 class Pi:
@@ -31,9 +31,7 @@ class Configuration:
             m = re.search(r"Raspberry Pi (\d+)", model)
             if not m:
                 raise ValueError(f"Couldn't parse Pi model from {model!r}")
-            return Pi(
-                version=int(m.group(1))
-            )
+            return Pi(version=int(m.group(1)))
 
     @classmethod
     def is_profiling_mode(cls) -> bool:
