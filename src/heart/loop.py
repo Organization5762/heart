829604180f37b30eb385eacd8c17939e315daef4
Michael Lampe <viper.toyed_07@icloud.com>
2025-05-17 23:16:29 -0700

    Lampe/tixyland (#77)
    * Fmt
    
    * Tixyland is cool
    
    * Tixyland
    
    * fmt

--- src/heart/loop.py
+++ src/heart/loop.py
@@ -2,11 +2,12 @@ import os
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 
+import importlib
 from typing import Annotated
 
 import typer
 
-from heart.device import Cube
+from heart.device import Cube, Device
 from heart.device.local import LocalScreen
 from heart.display.color import Color
 from heart.display.renderers.color import RenderColor
@@ -22,18 +23,7 @@ logger = get_logger(__name__)
 app = typer.Typer()
 
 
-@app.command()
-def run(
-    configuration: Annotated[str, typer.Option("--configuration")] = "lib_2025",
-    add_low_power_mode: bool = typer.Option(
-        True, "--add-low-power-mode", help="Add a low power mode"
-    ),
-) -> None:
-    registry = ConfigurationRegistry()
-    configuration_fn = registry.get(configuration)
-    if configuration_fn is None:
-        raise Exception(f"Configuration '{configuration}' not found in registry")
-
+def _get_device(x11_forward: bool) -> Device:
     # TODO: Add a way of adding orientation either from Config or `run`
     orientation = Cube.sides()
     if Configuration.is_pi():
@@ -55,9 +45,26 @@ def run(
             device = LEDMatrix(orientation=orientation)
     else:
         device = LocalScreen(width=64, height=64, orientation=orientation)
+    return device
+
+
+@app.command()
+def run(
+    configuration: Annotated[str, typer.Option("--configuration")] = "lib_2025",
+    add_low_power_mode: bool = typer.Option(
+        True, "--add-low-power-mode", help="Add a low power mode"
+    ),
+    x11_forward: bool = typer.Option(
+        False, "--x11-forward", help="Use X11 forwarding for RGB display"
+    ),
+) -> None:
+    registry = ConfigurationRegistry()
+    configuration_fn = registry.get(configuration)
+    if configuration_fn is None:
+        raise Exception(f"Configuration '{configuration}' not found in registry")
 
     manager = PeripheralManager()
-    loop = GameLoop(device=device, peripheral_manager=manager)
+    loop = GameLoop(device=_get_device(x11_forward), peripheral_manager=manager)
     configuration_fn(loop)
 
     ## ============================= ##
@@ -66,6 +73,31 @@ def run(
     # Retain an empty loop for "lower power" mode
     if add_low_power_mode:
         loop.app_controller.add_sleep_mode()
+    loop.start()
+
+
+@app.command(
+    name="test-renderer",
+)
+def test_renderer(
+    renderer_name: Annotated[
+        str, typer.Option("--renderer", help="Renderer class name")
+    ] = "heart.display.renderers.tixyland:Tixyland",
+    add_low_power_mode: bool = typer.Option(
+        True, "--add-low-power-mode", help="Add a low power mode"
+    ),
+    x11_forward: bool = typer.Option(
+        False, "--x11-forward", help="Use X11 forwarding for RGB display"
+    ),
+) -> None:
+    module_name, class_name = renderer_name.split(":")
+    module = importlib.import_module(module_name)
+    renderer_class = getattr(module, class_name)
+    renderer = renderer_class()
+    loop = GameLoop(
+        device=_get_device(x11_forward), peripheral_manager=PeripheralManager()
+    )
+    loop.app_controller.add_mode(renderer)
     loop.start()
 
 
---
+++ src/heart/programs/configurations/tixyland.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from heart.display.renderers.tixyland import Tixyland
+from heart.environment import GameLoop
+from heart.navigation import MultiScene
+
+
+def pattern_numpy(t: float, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+    t_i = int(t)
+    val = (Y - 2 * t_i) * (X - 2 - t_i)
+    return val
+
+
+def configure(loop: GameLoop) -> None:
+    mode = loop.add_mode()
+    mode.add_renderer(
+        MultiScene(
+            [
+                Tixyland(fn=lambda t, i, x, y: np.sin(y / 8 + t)),
+                Tixyland(fn=lambda t, i, x, y: np.random.rand(*x.shape) < 0.1),
+                Tixyland(fn=lambda t, i, x, y: np.random.rand(*x.shape)),
+                Tixyland(fn=lambda t, i, x, y: np.sin(np.ones(x.shape) * t)),
+                Tixyland(fn=lambda t, i, x, y: y - t * t),
+                Tixyland(
+                    fn=lambda t, i, x, y: np.sin(
+                        t
+                        - np.sqrt((x - x.shape[0] / 2) ** 2 + (y - y.shape[1] / 2) ** 2)
+                    )
+                ),
+                Tixyland(fn=lambda t, i, x, y: np.sin(y / 8 + t)),
+                Tixyland(fn=lambda t, i, x, y: pattern_numpy(t, x, y)),
+            ]
+        )
+    )
