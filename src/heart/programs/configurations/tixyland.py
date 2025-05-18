8658a76aa010747db2ddd51e39c4c6ea9c03af81
Michael Lampe <michaelthelampe@gmail.com>
2025-05-17 23:14:22 -0700

    Tixyland

--- src/heart/programs/configurations/tixyland.py
+++ src/heart/programs/configurations/tixyland.py
@@ -3,20 +3,25 @@ from heart.environment import GameLoop
 from heart.display.renderers.tixyland import Tixyland
 from heart.navigation import MultiScene
 
+def pattern_numpy(t: float, X: np.ndarray, Y: np.ndarray) -> np.ndarray:
+    t_i = int(t)
+    val = (Y - 2 * t_i) * (X - 2 - t_i)
+    return val
+
 def configure(loop: GameLoop) -> None:
     mode = loop.add_mode()
     mode.add_renderer(
         MultiScene(
             [
-                # Tixyland(
-                #     fn=lambda t, i, x, y: np.sin(y / 8 + t)
-                # ),
-                # Tixyland(
-                #     fn=lambda t, i, x, y: np.random.rand(*x.shape) < 0.1
-                # ),
-                # Tixyland(
-                #     fn=lambda t, i, x, y: np.random.rand(*x.shape)
-                # ),
+                Tixyland(
+                    fn=lambda t, i, x, y: np.sin(y / 8 + t)
+                ),
+                Tixyland(
+                    fn=lambda t, i, x, y: np.random.rand(*x.shape) < 0.1
+                ),
+                Tixyland(
+                    fn=lambda t, i, x, y: np.random.rand(*x.shape)
+                ),
                 Tixyland(
                     fn=lambda t, i, x, y: np.sin(np.ones(x.shape) * t)
                 ),
@@ -30,49 +35,8 @@ def configure(loop: GameLoop) -> None:
                     fn=lambda t, i, x, y: np.sin(y/8 + t)
                 ),
                 Tixyland(
-                    fn=lambda t, i, x, y: (y-4*t|0) * (x-2-t|0)
+                    fn=lambda t, i, x, y: pattern_numpy(t, x, y)
                 ),
-                # Tixyland(
-                #     fn=lambda t, i, x, y: 4 * t & i & x & y
-                # )
             ]
         )
     )
-    # mode.add_renderer(
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: np.random() < 0.1
-    #     )
-    # )
-    # mode.add_renderer(
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: np.random()
-    #     )
-    # )
-    # mode.add_renderer(
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: np.sin(t)
-    #     )
-    # )
-    # mode.add_renderer(
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: y - t
-    #     ),
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: y - t * t
-    #     ),
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: np.sin(t - np.sqrt((x - 7.5) ** 2 + (y - 6) ** 2))
-    #     ),
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: np.sin(y/8 + t)
-    #     ),
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: -(x>t & y>t & x<15-t & y<15-t)
-    #     ),
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: (y-4*t|0) * (x-2-t|0)
-    #     ),
-    #     Tixyland(
-    #         fn=lambda t, i, x, y: 4 * t & i & x & y
-    #     )
-    # )
