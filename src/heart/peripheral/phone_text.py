69929d513a7a6e9935cf819d9d842a6da40efc60
Michael Lampe <viper.toyed_07@icloud.com>
2025-11-01 07:56:17 -0700

    Refine peripheral infrastructure and detection

--- src/heart/peripheral/phone_text.py
+++ src/heart/peripheral/phone_text.py
@@ -123,6 +123,6 @@ class PhoneText(Peripheral):
             # Clear the buffer for the next message
             self._buffer.clear()
 
-    @staticmethod
-    def detect() -> Iterator["PhoneText"]:
-        yield PhoneText()
+    @classmethod
+    def detect(cls) -> Iterator["PhoneText"]:
+        yield cls()
