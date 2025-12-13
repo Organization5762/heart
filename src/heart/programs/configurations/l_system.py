3db62f82e7595fe7a3628360b05f2e81a5dfa74b
Michael Lampe <viper.toyed_07@icloud.com>
2025-12-14 03:29:28 +0900

    Refactor L-system renderer into provider/state/renderer

--- src/heart/programs/configurations/l_system.py
+++ src/heart/programs/configurations/l_system.py
@@ -1,7 +1,9 @@
-from heart.display.renderers.l_system import LSystem
+from heart.display.renderers.l_system import LSystem, LSystemStateProvider
 from heart.environment import GameLoop
 
 
 def configure(loop: GameLoop) -> None:
     mode = loop.add_mode()
-    mode.add_renderer(LSystem())
+    mode.add_renderer(
+        LSystem(builder=loop.context_container.resolve(LSystemStateProvider))
+    )
