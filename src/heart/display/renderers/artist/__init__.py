a9b1ef5dd339f80d98ee6638d54248a94d2c3a36
Michael Lampe <viper.toyed_07@icloud.com>
2025-12-14 04:02:11 +0900

    Refactor artist and kirby renderers into packages

---
+++ src/heart/display/renderers/artist/__init__.py
@@ -0,0 +1,4 @@
+from heart.display.renderers.artist.provider import \
+    ArtistStateProvider as ArtistStateProvider
+from heart.display.renderers.artist.renderer import ArtistScene as ArtistScene
+from heart.display.renderers.artist.state import ArtistState as ArtistState
