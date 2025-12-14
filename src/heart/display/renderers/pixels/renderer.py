a47576240be330e93859af115f2fa7d2eefa4f78
Michael Lampe <viper.toyed_07@icloud.com>
2025-12-15 03:43:16 +0900

    Refactor legacy renderers into provider packages

--- src/heart/display/renderers/pixels/renderer.py
+++ src/heart/display/renderers/pixels/renderer.py
@@ -1,5 +1,4 @@
 import random
-from dataclasses import dataclass
 
 import pygame
 
@@ -7,30 +6,17 @@ from heart import DeviceDisplayMode
 from heart.device import Orientation
 from heart.display.color import Color
 from heart.display.renderers import AtomicBaseRenderer
+from heart.display.renderers.pixels.provider import (BorderStateProvider,
+                                                     RainStateProvider,
+                                                     SlinkyStateProvider)
+from heart.display.renderers.pixels.state import (BorderState, RainState,
+                                                  SlinkyState)
 from heart.peripheral.core.manager import PeripheralManager
 
 
-@dataclass
-class BorderState:
-    color: Color
-
-
-@dataclass
-class RainState:
-    starting_point: int = 0
-    current_y: int = 0
-
-
-@dataclass
-class SlinkyState:
-    starting_point: int = 0
-    current_y: int = 0
-
-
 class Border(AtomicBaseRenderer[BorderState]):
-    def __init__(self, width: int, color: Color | None = None) -> None:
-        # TODO: This whole freaking this is broken
-        self._initial_color = color or Color.random()
+    def __init__(self, width: int, color: Color | None = None, provider: BorderStateProvider | None = None) -> None:
+        self.provider = provider or BorderStateProvider(color)
         AtomicBaseRenderer.__init__(self)
         self.device_display_mode = DeviceDisplayMode.FULL
         self.width = width
@@ -42,17 +28,13 @@ class Border(AtomicBaseRenderer[BorderState]):
         orientation: Orientation,
     ) -> None:
         width, height = window.get_size()
-
-        # Draw the border
         color_value = self.state.color._as_tuple()
         for x in range(width):
-            # Top and Bottom
             for y in range(self.width):
                 window.set_at((x, y), color_value)
                 window.set_at((x, height - 1 - y), color_value)
 
         for y in range(height):
-            # Left and Right
             for x in range(self.width):
                 window.set_at((x, y), color_value)
                 window.set_at((width - 1 - x, y), color_value)
@@ -62,17 +44,22 @@ class Border(AtomicBaseRenderer[BorderState]):
         window: pygame.Surface,
         clock: pygame.time.Clock,
         peripheral_manager: PeripheralManager,
-        orientation: Orientation
-    ):
-        return BorderState(color=self._initial_color)
+        orientation: Orientation,
+    ) -> BorderState:
+        return self.provider.create_initial_state(
+            window=window,
+            clock=clock,
+            peripheral_manager=peripheral_manager,
+            orientation=orientation,
+        )
 
     def set_color(self, color: Color) -> None:
         self.update_state(color=color)
 
 
 class Rain(AtomicBaseRenderer[RainState]):
-    def __init__(self) -> None:
-        # TODO: This whole freaking this is broken
+    def __init__(self, provider: RainStateProvider | None = None) -> None:
+        self.provider = provider or RainStateProvider()
         AtomicBaseRenderer.__init__(self)
         self.device_display_mode = DeviceDisplayMode.FULL
         self.l = 8
@@ -83,15 +70,16 @@ class Rain(AtomicBaseRenderer[RainState]):
         window: pygame.Surface,
         clock: pygame.time.Clock,
         peripheral_manager: PeripheralManager,
-        orientation: Orientation
-    ):
-        initial_y = random.randint(0, 20)
-        return RainState(
-            window.get_width(),
-            current_y=initial_y
+        orientation: Orientation,
+    ) -> RainState:
+        return self.provider.create_initial_state(
+            window=window,
+            clock=clock,
+            peripheral_manager=peripheral_manager,
+            orientation=orientation,
         )
 
-    def _change_starting_point(self, width, *, current_y: int = 0) -> None:
+    def _change_starting_point(self, width: int) -> None:
         self.update_state(
             starting_point=random.randint(0, width),
             current_y=self.state.current_y,
@@ -104,13 +92,8 @@ class Rain(AtomicBaseRenderer[RainState]):
         orientation: Orientation,
     ) -> None:
         width, height = window.get_size()
-
-        # Move one unit
         new_y = self.state.current_y + 1
         starting_point = self.state.starting_point
-
-        # Now draw a rain drop
-        # It should decrease the saturation, but also dim
 
         for i in range(self.l):
             color = self.starting_color.dim(fraction=i / self.l)
@@ -123,8 +106,8 @@ class Rain(AtomicBaseRenderer[RainState]):
 
 
 class Slinky(AtomicBaseRenderer[SlinkyState]):
-    def __init__(self) -> None:
-        # TODO: This whole freaking this is broken
+    def __init__(self, provider: SlinkyStateProvider | None = None) -> None:
+        self.provider = provider or SlinkyStateProvider()
         AtomicBaseRenderer.__init__(self)
         self.device_display_mode = DeviceDisplayMode.FULL
         self.l = 10
@@ -135,14 +118,16 @@ class Slinky(AtomicBaseRenderer[SlinkyState]):
         window: pygame.Surface,
         clock: pygame.time.Clock,
         peripheral_manager: PeripheralManager,
-        orientation: Orientation
-    ):
-        return SlinkyState(
-            starting_point=random.randint(0, window.get_size()[0]),
-            current_y=random.randint(0, 20)
+        orientation: Orientation,
+    ) -> SlinkyState:
+        return self.provider.create_initial_state(
+            window=window,
+            clock=clock,
+            peripheral_manager=peripheral_manager,
+            orientation=orientation,
         )
 
-    def _change_starting_point(self, width, *, current_y: int = 0) -> None:
+    def _change_starting_point(self, width: int) -> None:
         self.update_state(
             starting_point=random.randint(0, width),
             current_y=self.state.current_y,
@@ -155,39 +140,23 @@ class Slinky(AtomicBaseRenderer[SlinkyState]):
         orientation: Orientation,
     ) -> None:
         width, height = window.get_size()
-
-        # Move one unit
         new_y = self.state.current_y + 1
         starting_point = self.state.starting_point
 
-        # Now draw a rain drop
-        # It should decrease the saturation, but also dim
-
         window.set_at((starting_point, new_y), self.starting_color)
-        f = self.starting_color.dim(fraction=1 / self.l)
-        window.set_at((starting_point + 1, new_y), f)
-        window.set_at((starting_point - 1, new_y), f)
+        dimmed_color = self.starting_color.dim(fraction=1 / self.l)
+        window.set_at((starting_point + 1, new_y), dimmed_color)
+        window.set_at((starting_point - 1, new_y), dimmed_color)
         for i in range(self.l):
-            # Make a triangle:
-            # Brightness
-            #    -----
-            # --/     \--
-
-            # I want this to transition from Orange to Yellow as it moves, going through black as a center point
-            final_color = list(self.starting_color)
             final_color = self.starting_color.dim(fraction=i / self.l)
             window.set_at((starting_point, new_y + i), final_color)
             window.set_at((starting_point, new_y - i), final_color)
             if i < 3:
-                f = self.dim(self.starting_color, fraction=(i + 1) / self.l)
-                window.set_at((starting_point + 1, new_y + i), f)
-                window.set_at((starting_point - 1, new_y - i), f)
+                more_dim = self.starting_color.dim(fraction=(i + 1) / self.l)
+                window.set_at((starting_point + 1, new_y + i), more_dim)
+                window.set_at((starting_point - 1, new_y - i), more_dim)
 
         if new_y > height:
             self._change_starting_point(width=width)
         else:
             self.update_state(current_y=new_y)
-
-
-## More Ideas
-# - Comets
---
+++ src/heart/display/renderers/yolisten/__init__.py
@@ -0,0 +1,5 @@
+from heart.display.renderers.yolisten.provider import \
+    YoListenStateProvider  # noqa: F401
+from heart.display.renderers.yolisten.renderer import \
+    YoListenRenderer  # noqa: F401
+from heart.display.renderers.yolisten.state import YoListenState  # noqa: F401
