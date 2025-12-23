a6dc3042594ed25a419d20e7e271229b9e8f5682
Michael Lampe <viper.toyed_07@icloud.com>
2025-12-24 03:15:05 +0900

    Remove __all__ re-exports in renderer packages (#393)

--- src/heart/renderers/spritesheet/__init__.py
+++ src/heart/renderers/spritesheet/__init__.py
@@ -1,17 +1,13 @@
-from heart.renderers.spritesheet.provider import SpritesheetProvider
-from heart.renderers.spritesheet.renderer import (SpritesheetLoop,
-                                                  create_spritesheet_loop)
-from heart.renderers.spritesheet.state import (BoundingBox, FrameDescription,
-                                               LoopPhase, Size,
-                                               SpritesheetLoopState)
-
-__all__ = [
-    "BoundingBox",
-    "FrameDescription",
-    "LoopPhase",
-    "Size",
-    "SpritesheetLoopState",
-    "SpritesheetLoop",
-    "create_spritesheet_loop",
-    "SpritesheetProvider",
-]
+from heart.renderers.spritesheet.provider import \
+    SpritesheetProvider as SpritesheetProvider
+from heart.renderers.spritesheet.renderer import \
+    SpritesheetLoop as SpritesheetLoop
+from heart.renderers.spritesheet.renderer import \
+    create_spritesheet_loop as create_spritesheet_loop
+from heart.renderers.spritesheet.state import BoundingBox as BoundingBox
+from heart.renderers.spritesheet.state import \
+    FrameDescription as FrameDescription
+from heart.renderers.spritesheet.state import LoopPhase as LoopPhase
+from heart.renderers.spritesheet.state import Size as Size
+from heart.renderers.spritesheet.state import \
+    SpritesheetLoopState as SpritesheetLoopState
