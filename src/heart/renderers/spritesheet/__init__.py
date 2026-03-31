d017cd5a81cf38cb858df4a3825e7deef0ca51c8
Michael Lampe <viper.toyed_07@icloud.com>
2026-03-31 12:52:25 +0900

    Simplify renderer core abstractions (#781)

--- src/heart/renderers/spritesheet/__init__.py
+++ src/heart/renderers/spritesheet/__init__.py
@@ -2,8 +2,6 @@ from heart.renderers.spritesheet.provider import \
     SpritesheetProvider as SpritesheetProvider
 from heart.renderers.spritesheet.renderer import \
     SpritesheetLoop as SpritesheetLoop
-from heart.renderers.spritesheet.renderer import \
-    create_spritesheet_loop as create_spritesheet_loop
 from heart.renderers.spritesheet.state import BoundingBox as BoundingBox
 from heart.renderers.spritesheet.state import \
     FrameDescription as FrameDescription
