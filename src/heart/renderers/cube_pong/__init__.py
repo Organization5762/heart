2d820c5e285f247e43104312f95a00d12074d358
Seb <sebastien@picnichealth.com>
2026-05-19 17:14:04 -0700

    Polish Tetris and Cube Pong controller rendering

--- src/heart/renderers/cube_pong/__init__.py
+++ src/heart/renderers/cube_pong/__init__.py
@@ -1,7 +1,18 @@
 from heart.renderers.cube_pong.renderer import CubePongRenderer
+from heart.display.color import Color
+from heart.renderers.text import TextRendering
 from heart.runtime.game_loop import GameLoop
 
 
 def add_cube_pong_mode(loop: GameLoop) -> None:
-    mode = loop.add_mode("cube\npong")
+    mode = loop.add_mode(
+        TextRendering(
+            text=["cube\npong"],
+            font="Grand9K Pixel.ttf",
+            font_size=12,
+            color=Color(255, 105, 180),
+            y_location=15 / 64,
+            line_spacing_px=-2,
+        )
+    )
     mode.add_renderer(CubePongRenderer())
